d9df2ebee1282bf7bf552054d5d54c65b285ef1f
greyje <petrov3466@mail.ru>
2022-12-09 15:34:51 +0500

    pyqt5

--- manager(sqlite).py
+++ manager(sqlite).py
@@ -1,3 +1,4 @@
+import calendar
 import os
 import datetime
 import time
@@ -9,7 +10,7 @@ import pprint
 import sqlite3
 from database_query import if_not_exists
 from PyQt5 import QtWidgets, QtCore, QtGui
-from PyQt5.QtWidgets import QDialog, QApplication, QWidget, QMainWindow, QVBoxLayout
+from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QAction
 
 
 class MainWindow(QWidget):
@@ -30,19 +31,18 @@ class MainWindow(QWidget):
         self.setLayout(layout)
 
     def go_to_login(self):
-        window2 = LogInWindow()
-        widget.addWidget(window2)
-        widget.setCurrentIndex(widget.currentIndex() + 1)
-
+        self.LGW = LogInWindow()
+        self.LGW.show()
 
     def go_to_signup(self):
-        window3 = SignUpWindow()
-        widget.addWidget(window3)
-        widget.setCurrentIndex(widget.currentIndex() + 1)
+        self.SUW = SignUpWindow()
+        self.SUW.show()
+
 
 class LogInWindow(QWidget):
     def __init__(self):
         super(LogInWindow, self).__init__()
+        self.setWindowTitle("Log in")
         self.usernameLabel = QtWidgets.QLabel("Username")
         self.passwordLabel = QtWidgets.QLabel("Password")
         self.backButton = QtWidgets.QPushButton('Back')
@@ -50,7 +50,7 @@ class LogInWindow(QWidget):
         self.errorLabel = QtWidgets.QLabel('The email or password is incorrect.')
         self.usernameLine = QtWidgets.QLineEdit()
         self.passwordLine = QtWidgets.QLineEdit()
-        self.backButton.clicked.connect(self.go_back)
+        # self.backButton.clicked.connect(self.go_back)
         self.errorLabel.setStyleSheet("color : 'red'")
         self.passwordLine.setEchoMode(QtWidgets.QLineEdit.Password)
         self.loginButton.clicked.connect(self.login_function)
@@ -59,7 +59,7 @@ class LogInWindow(QWidget):
     def initUI(self):
         layout = QtWidgets.QGridLayout()
         layout.setAlignment(QtCore.Qt.AlignCenter)
-        #buttonLayout.setAlignment(QtCore.Qt.AlignRight | QtCore.Qt.AlignBottom)
+        # buttonLayout.setAlignment(QtCore.Qt.AlignRight | QtCore.Qt.AlignBottom)
         self.passwordLine.setFixedSize(200, 25)
         self.usernameLine.setFixedSize(200, 25)
         layout.addWidget(self.usernameLabel)
@@ -72,9 +72,9 @@ class LogInWindow(QWidget):
         self.errorLabel.hide()
         self.setLayout(layout)
 
-    def go_back(self):
-        os.system('cls')
-        widget.setCurrentIndex(widget.currentIndex() - 1)
+    # def go_back(self):
+    # os.system('cls')
+    # widget.setCurrentIndex(widget.currentIndex() - 1)
 
     def login_function(self):
         username = self.usernameLine.text()
@@ -85,11 +85,16 @@ class LogInWindow(QWidget):
         else:
             if SignUp().user_exists(username):
                 if Tasks().compare_data(username, password):
-                    print("func works") #Tasks().compare_data(username, password) doesnt work
-                    self.errorLabel.setText("Successfully logged in.")
-                    self.errorLabel.setStyleSheet("color : 'green'")
-                    self.errorLabel.show()
+                    # self.errorLabel.setText("Successfully logged in.")
+                    # self.errorLabel.setStyleSheet("color : 'green'")
+                    # self.errorLabel.show()
+                    # TaskManagerUI().data_receiver(username, password)
+                    username_session.append(username)
+                    password_session.append(password)
                     self.go_to_task_manager()
+                    self.close()
+                    MW.close()
+
                 else:
                     self.errorLabel.setText("Invalid username or password")
                     self.errorLabel.show()
@@ -98,11 +103,14 @@ class LogInWindow(QWidget):
                 self.errorLabel.show()
 
     def go_to_task_manager(self):
-        pass
+        self.TMUI = TaskManagerUI()
+        self.TMUI.show()
+
 
 class SignUpWindow(QWidget):
     def __init__(self):
         super(SignUpWindow, self).__init__()
+        self.setWindowTitle('Sign up')
         self.usernameLabel = QtWidgets.QLabel("Username")
         self.passwordLabel = QtWidgets.QLabel("Password")
         self.passwordLabel2 = QtWidgets.QLabel("Confirm password")
@@ -113,7 +121,7 @@ class SignUpWindow(QWidget):
         self.passwordLine2 = QtWidgets.QLineEdit()
         self.errorLabel = QtWidgets.QLabel('ERROR')
         self.errorLabel.setStyleSheet("color : 'red'")
-        self.backButton.clicked.connect(self.go_back)
+        # self.backButton.clicked.connect(self.go_back)
         self.createButton.clicked.connect(self.create_func)
         self.initUI()
 
@@ -136,8 +144,8 @@ class SignUpWindow(QWidget):
         self.errorLabel.hide()
         self.setLayout(layout)
 
-    def go_back(self):
-        widget.setCurrentIndex(widget.currentIndex() - 1)
+    # def go_back(self):
+    # widget.setCurrentIndex(widget.currentIndex() - 1)
 
     def create_func(self):
         username = self.usernameLine.text()
@@ -147,6 +155,7 @@ class SignUpWindow(QWidget):
         if password != password1:
             self.errorLabel.setText("Passwords are not matching")
             self.errorLabel.show()
+            QtCore.QTimer.singleShot(5, SignUpWindow.close)
             LogInWindow().go_to_task_manager()
         else:
             if SignUp().user_exists(username):
@@ -154,35 +163,185 @@ class SignUpWindow(QWidget):
                 self.errorLabel.show()
             else:
                 DataBase().database_sign_up(username, password)
-                self.errorLabel.setText('Succesfully signed up!')
-                self.errorLabel.setStyleSheet("color : 'green'")
-                self.errorLabel.show()
+                # self.errorLabel.setText('Succesfully signed up!')
+                # self.errorLabel.setStyleSheet("color : 'green'")
+                # self.errorLabel.show()
+                self.close()
         LogInWindow().go_to_task_manager()
 
-class TaskManagerUI(QWidget):
-    pass
+
+class TaskManagerUI(QMainWindow):
+    def __init__(self):
+        super(TaskManagerUI, self).__init__()
+        self.setFixedSize(800, 400)
+        self.setWindowTitle('Task manager')
+
+        self.menu = QtWidgets.QMenu
+        self.toolbar = QtWidgets.QToolBar()
+        self.list = QtWidgets.QListWidget()
+
+        self.addToolBar(self.toolbar)
+        self.toolbar.setMovable(False)
+        self.clear_button = QtWidgets.QPushButton('clear')
+        self.button_action = QAction("All tasks", self)
+        self.button_action.triggered.connect(self.all_tasks)
+        self.button_action2 = QAction("Add task", self)
+        self.button_action2.triggered.connect(self.add_task)
+        self.button_action3 = QAction("Tasks status", self)
+        self.button_action3.triggered.connect(self.tasks_status)
+        self.button_action4 = QAction("Update status", self)
+        self.button_action4.triggered.connect(self.update_status)
+
+        self.toolbar.setStyleSheet('background-color: white;')
+        self.toolbar.addAction(self.button_action)
+        self.toolbar.addSeparator()
+        self.toolbar.addAction(self.button_action2)
+        self.toolbar.addSeparator()
+        self.toolbar.addAction(self.button_action3)
+        self.toolbar.addSeparator()
+        self.toolbar.addAction(self.button_action4)
+
+        self.usernameLine = QtWidgets.QLineEdit()
+        self.passwordLine = QtWidgets.QLineEdit()
+
+        self.list.setStyleSheet('QListWidget::item {color:white;}')
+        self.setCentralWidget(self.list)
+
+    def clear_list(self):
+        self.list.clear()
+
+    def re_tasks(self):
+        username = username_session[0]
+        return DataBase().database_all_tasks(username)
+
+    def all_tasks(self):
+        for i in self.re_tasks():
+            # print(str(i))
+            self.list.addItem(str(i))
+
+    def add_task(self):
+        self.dialog = DialogWindow()
+        self.dialog.show()
+
+    def tasks_status(self):
+        # DataBase().database_task_status(username, password, status_choice)
+        pass
+
+    def update_status(self):
+        pass
+
+
+# dialog window for add task func
+class DialogWindow(QWidget):
+    def __init__(self):
+        super(DialogWindow, self).__init__()
+        self.setWindowTitle('Add task')
+        self.setFixedSize(400, 300)
+
+        self.add_button = QtWidgets.QPushButton('Add')
+        self.date_edit = QtWidgets.QDateEdit()
+        self.date_edit.setCalendarPopup(True)
+        self.date_edit.setFixedHeight(30)
+        self.date_edit.setStyleSheet('QDateEdit {color: #D3D3D3}')
+        self.date_edit.setDateTime(QtCore.QDateTime.currentDateTime())
+
+        self.user_input = QtWidgets.QLineEdit()
+
+        self.label = QtWidgets.QLabel('Input your task')
+        self.label.setStyleSheet('QLabel{font-size: 15pt}')
+        self.label.setAlignment(QtCore.Qt.AlignCenter)
+
+        self.check_box = QtWidgets.QCheckBox()
+        self.check_box.setText('Add steps ?')
+        self.check_box.setStyleSheet('QCheckBox {color: #D3D3D3;}')
+        self.check_box.stateChanged.connect(lambda: self.checked())
+
+        self.error_label = QtWidgets.QLabel('Input integer')
+        self.error_label.setStyleSheet('QLabel {color: red}')
+
+        layout = QtWidgets.QGridLayout()
+        layout.addWidget(self.label)
+        layout.addWidget(self.user_input)
+        layout.setSpacing(20)
+        layout.addWidget(self.date_edit)
+        layout.setSpacing(20)
+        layout.addWidget(self.check_box)
+        layout.addWidget(self.add_button)
+
+        self.setLayout(layout)
+
+        self.temp = self.date_edit.date()
+        date.append(self.temp.toPyDate())
+        content.append(self.user_input.text())
+
+        self.add_button.clicked.connect(self.all_task_data_received())
+
+    def checked(self):
+        if self.check_box.isChecked():
+            self.dialog()
+
+    def dialog(self):
+        self.ask = AskUser()
+        self.ask.show()
+
+    def all_task_data_received(self):
+        steps = ''
+        print(date, content)
+        self.add_button.clicked.connect(DataBase().database_add_task())
+
+
+class AskUser(QWidget):
+    def __init__(self):
+        super(AskUser, self).__init__()
+        self.resize(300, 150)
+        self.setWindowTitle('Steps')
+        self.items = []
+        self.item_count = 0
+
+        label = QtWidgets.QLabel("Number of steps")
+        self.accept_btn = QtWidgets.QPushButton('Accept')
+        self.spinBox = QtWidgets.QSpinBox(self)
+        self.spinBox.setRange(0, 5)
+        self.spinBox.valueChanged.connect(self.set_item_count)
+        self.spinBox.setStyleSheet('QSpinBox {color: #D3D3D3}')
+        groupBox = QtWidgets.QGroupBox("Input steps")
+        groupBox.setStyleSheet('QGroupBox{color: #D3D3D3}')
+        self.item_layout = QtWidgets.QVBoxLayout(groupBox)
+        self.item_layout.addStretch(2)
+
+        g_layout = QtWidgets.QGridLayout(self)
+        g_layout.addWidget(label, 0, 0, 1, 2)
+        g_layout.addWidget(self.spinBox, 0, 2, 1, 1)
+        g_layout.addWidget(groupBox, 2, 0, 5, 3)
+        g_layout.addWidget(self.accept_btn)
+
+    def set_item_count(self, new_count: int):
+        lineEdit = QtWidgets.QLineEdit
+        n_items = len(self.items)
+        for i in range(n_items, new_count):
+            item = lineEdit(self)
+            self.items.append(item)
+            self.item_layout.insertWidget(n_items, item)
+        for i in range(self.item_count, new_count):
+            self.item_layout.itemAt(i).widget().show()
+        for i in range(new_count, self.item_count):
+            self.item_layout.itemAt(i).widget().hide()
+        self.item_count = new_count
+
+        if n_items > new_count:
+            self.items = self.items[:-1]
+
+        steps = []
+        for i in self.items:
+            steps.append(i.text())
+        print(steps)
+
+        self.accept_btn.clicked.connect()
+
 
 class SignUp:
     def __init__(self):
         self.con = sqlite3.connect("database/database.db")
-
-    def sign_up(self):
-        os.system('cls')
-        print("Введите имя и пароль для регистрации")
-        username = input("Введите имя: ")
-        password = input("Введите пароль: ")
-        password1 = input('Подтвердите пароль: ')
-        Tasks().compare_data(username, password)
-        if password != password1:
-            print("Пароли не совпадают!")
-            TaskManager().main()
-        else:
-            if self.user_exists(username):
-                print("Такой пользователь уже существует")
-            else:
-                DataBase().database_sign_up(username, password)
-                print('Регистрация прошла успешно!')
-        TaskManager().main()
 
     def user_exists(self, username):
         with self.con as db:
@@ -204,27 +363,15 @@ class DataBase:
             cur.execute('INSERT INTO usersdata(username, password) VALUES (?, ?)',
                         Tasks().func_user_info(username, password))
 
-    def database_add_task(self, username):
+    def database_add_task(self):
+        username = username_session[0]
         with self.con as db:
             status = 'ongoing'
-            content = input('Введите задачу, которую хотите добавить в список:\n ')
-            d = input('Введите дату (ГГГГ-ММ-ДД): ')
-            date = datetime.strptime(d, '%Y-%m-%d')
-            steps = 'NULL'
+            #d = input('Введите дату (ГГГГ-ММ-ДД): ')
+            #date = datetime.strptime(d, '%Y-%m-%d')
             cur = db.cursor()
-            option = input(
-                'Нужно ли добавить шаги ? \n<Enter>, чтобы пропустить добавление шагов \n''да'', если добавить \n')
-            if len(option) > 1:
-                if option.lower() == 'да':
-                    j = int(input('Введите сколько шагов нужно добавить: '))
-                    steps = []
-                    for i in range(1, j + 1):
-                        step = input("Введите шаг :")
-                        steps.append(step)
-                    steps_string = ', '.join([str(item) for item in steps])
-                elif option == 'нет' or len(option) < 1 or 'Нет':
-                    steps = 'NULL'
-            if steps == 'NULL':
+            print(username, date, content, status, status_time)
+            """if steps == '':
                 cur.execute(
                     'INSERT INTO userstasks(username, date, content, status, status_time) VALUES (?, ?, ?, ?, ?)',
                     (username, date, content, status, time.ctime()))
@@ -232,18 +379,20 @@ class DataBase:
                 cur.execute(
                     'INSERT INTO userstasks(username, date, content, steps, status, status_time) VALUES (?, ?, ?, ?, ?,'
                     ' ?)',
-                    (username, date, content, steps_string, status, time.ctime()))
+                    (username, date, content, steps, status, time.ctime()))"""
 
-    def database_all_tasks(self, username, execute):
+    def database_all_tasks(self, username):
         with self.con as db:
+            row_list = []
             cur = db.cursor()
             execute = cur.execute('SELECT * from userstasks WHERE username = ?',
                                   (Tasks().func_username_container(username)))
-            if Tasks().tasks_exists(username):
+            if not Tasks().tasks_exists(username):
                 for row in execute:
-                    print(row)
+                    row_list.append(row)
+                return row_list
             else:
-                print("У пользователя нет никаких задач!")
+                print("Not a single task was found")
 
     def database_task_status(self, username, status_choice, password):
         status = []
@@ -354,8 +503,7 @@ class DataBase:
             cur.execute("SELECT username, password FROM usersdata")
             data_list = cur.fetchall()
             for i in data_list:
-                Tasks().func_user_input(username, password)
-                if Tasks().func_user_input(username, password) == (i[0], i[1]):
+                if Tasks().func_user_input(username, password) == i:
                     return True
         return False
 
@@ -363,16 +511,6 @@ class DataBase:
 class Tasks:
     def __init__(self):
         self.con = sqlite3.connect("database/database.db")
-
-    def add_task(self, username, password):
-        # tid = str(uuid.uuid4().fields[-1])[:5]
-        DataBase().database_add_task(username)
-        TaskManager().task_manager(username, password)
-
-    def all_tasks(self, username, password):
-        print('Текущие задачи пользователя', username)
-        DataBase().database_all_tasks(username, password)
-        TaskManager().task_manager(username, password)
 
     def tasks_status(self, username, password):
         print('Задачи по статусу \n1- Просроченные задачи \n2- Задачи на сегодня \n3- Задачи на 3 дня')
@@ -405,7 +543,7 @@ class Tasks:
         return user_input
 
     def compare_data(self, username, password):
-        DataBase().database_compare_data(username, password)
+        return DataBase().database_compare_data(username, password)
 
     def return_function(self, choice):
         funcctions = {
@@ -453,34 +591,6 @@ class TaskManager:
         }
         return funcctions.get(int(choice))
 
-    def exitt(self):
-        sys.exit()
-
-    def task_manager(self, username, password):
-        print('Привет, ', username)
-        print(
-            "Менеджер задач \n1 - Все задачи \n2 - Добавить задачи \n3 - Задачи по статусу \n4 - Обновить статус задачи"
-            " \n5 - Возврат в меню")
-        choice = input('Введите пункт: ')
-        Tasks().return_function(choice)(username, password)
-        os.system('cls')
-
-    def log_in(self):
-        os.system('cls')
-        print('Введите данные для входа')
-        username = input('Имя: ')
-        password = input('Пароль: ')
-        if SignUp().user_exists(username):
-            if Tasks().compare_data(username, password):
-                print('Вы успешно вошли!')
-                self.task_manager(username, password)
-            else:
-                print('Введите корректные данные')
-                self.main()
-        else:
-            print('Пользователь не найден')
-            self.main()
-
 
 if __name__ == "__main__":
     # manager = TaskManager(user_name="awfawafgrg")
@@ -513,11 +623,14 @@ if __name__ == "__main__":
         }
         
     """
+    username_session = []
+    password_session = []
+    content = []
+    date = []
+    steps = []
     app.setStyleSheet(style)
-    window = MainWindow()
-    widget = QtWidgets.QStackedWidget()
-    widget.addWidget(window)
-    widget.setWindowTitle("Task manager")
-    widget.setFixedSize(400, 300)
-    widget.show()
+    MW = MainWindow()
+    MW.setWindowTitle("Task manager")
+    MW.setFixedSize(400, 300)
+    MW.show()
     sys.exit(app.exec_())
